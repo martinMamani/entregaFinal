4fdd6c8ff915185b746a188d67e1998eeaac13a3
martinMamani <mpm.mamani@gmail.com>
2023-09-26 09:48:31 -0300

    Se configuro las urls de todas las aplicacion con la url del proyecto

--- AppRegistro/views.py
+++ AppRegistro/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
+def inicio(request):
+    name = '<h1>inicio registro </h1>'
+    return HttpResponse(name)
--- entregaFinal/urls.py
+++ entregaFinal/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("dress/",include("AppIndumentaria.urls")),
+    path("login/",include("AppLogin.urls")),
+    path("profile/",include("AppPerfil.urls")),
+    path("register/",include("AppRegistro.urls")),
+    path("message/",include("AppMensaje.urls")),
 ]
