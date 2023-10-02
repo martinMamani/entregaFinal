32c8e1d843c23e0030988a3193db2e4ecffcc043
martinMamani <mpm.mamani@gmail.com>
2023-10-02 17:05:38 -0300

    creando el modelo de Producto

--- AppIndumentaria/models.py
+++ AppIndumentaria/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Producto(models.Model):
+    titulo = models.CharField(max_length=50)
+    subtitulo = models.CharField(max_length=50)
+    cuerpo = models.CharField(max_length=50)
+    autor = models.CharField(max_length=50)
+    fecha = models.DateTimeField()
+    url = models.ImageField()
